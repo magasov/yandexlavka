917bdb0f3094ed11a987e1a2f292d29e88456ccd
magasov <77aushev@mail.ru>
2025-02-23 14:35:38 +0300

    test

--- src/components/Catalog.vue
+++ src/components/Catalog.vue
@@ -2,51 +2,51 @@
     <div class="catalog">
 
 
-        <div class="catalog__main">
+        <div class="catalog__main"> 
             <h3>Каталог</h3>
 
             <nav class="links">
-                <a href="#"> <img
+                <a href="ready-meals"> <img
                         srcset="https://yastatic.net/avatars/get-grocery-goods/2998515/bccfe9f8e863453f9babb72d4d9b0d0e/40x40-webp 1x,https://yastatic.net/avatars/get-grocery-goods/2998515/bccfe9f8e863453f9babb72d4d9b0d0e/80x80-webp 2x"
                         src="https://yastatic.net/avatars/get-grocery-goods/2998515/bccfe9f8e863453f9babb72d4d9b0d0e/40x40-webp"
                         decoding="async" loading="eager" alt="Придумано Яндекс Лавкой" class="i1y1fbtp" width="40"
                         height="40" role="presentation">
                     Придумано Яндекс Лавкой</a>
-                <a href="#"><img
+                <a href="vegetables"><img
                         srcset="https://yastatic.net/avatars/get-grocery-goods/2750890/8ec7075a680646a08325b8cdf25565de/40x40-webp 1x,https://yastatic.net/avatars/get-grocery-goods/2750890/8ec7075a680646a08325b8cdf25565de/80x80-webp 2x"
                         src="https://yastatic.net/avatars/get-grocery-goods/2750890/8ec7075a680646a08325b8cdf25565de/40x40-webp"
                         decoding="async" loading="eager" alt="Готовая еда" class="i1y1fbtp" width="40" height="40"
                         role="presentation"> Готовая еда
 
                 </a>
-                <a href="#"><img
+                <a href="dairy"><img
                         srcset="https://yastatic.net/avatars/get-grocery-goods/2750890/9e5b5c3d91a7469a852c3163dfef5e3c/40x40-webp 1x,https://yastatic.net/avatars/get-grocery-goods/2750890/9e5b5c3d91a7469a852c3163dfef5e3c/80x80-webp 2x"
                         src="https://yastatic.net/avatars/get-grocery-goods/2750890/9e5b5c3d91a7469a852c3163dfef5e3c/40x40-webp"
                         decoding="async" loading="eager" alt="Овощной прилавок" class="i1y1fbtp" width="40" height="40"
                         role="presentation"> Овощной прилавок
                 </a>
-                <a href="#"><img
+                <a href="drinks"><img
                         srcset="https://yastatic.net/avatars/get-grocery-goods/2750890/7e196d537e5b46d79062a2ed5dfc4ccd/40x40-webp 1x,https://yastatic.net/avatars/get-grocery-goods/2750890/7e196d537e5b46d79062a2ed5dfc4ccd/80x80-webp 2x"
                         src="https://yastatic.net/avatars/get-grocery-goods/2750890/7e196d537e5b46d79062a2ed5dfc4ccd/40x40-webp"
                         decoding="async" loading="eager" alt="Молочный прилавок" class="i1y1fbtp" width="40" height="40"
                         role="presentation"> Молочный прилавок
 
                 </a>
-                <a href="#">
+                <a href="sweets">
                     <img srcset="https://yastatic.net/avatars/get-grocery-goods/2750890/8bb1188be474444eba935dc81ed58da3/40x40-webp 1x,https://yastatic.net/avatars/get-grocery-goods/2750890/8bb1188be474444eba935dc81ed58da3/80x80-webp 2x"
                         src="https://yastatic.net/avatars/get-grocery-goods/2750890/8bb1188be474444eba935dc81ed58da3/40x40-webp"
                         decoding="async" loading="eager" alt="Булочная" class="i1y1fbtp" width="40" height="40"
                         role="presentation"> Булочная
 
                 </a>
-                <a href="#"><img
+                <a href="meat-fish"><img
                         srcset="https://yastatic.net/avatars/get-grocery-goods/2750890/4f6a267fd6db40bc9ca316f035b976f2/40x40-webp 1x,https://yastatic.net/avatars/get-grocery-goods/2750890/4f6a267fd6db40bc9ca316f035b976f2/80x80-webp 2x"
                         src="https://yastatic.net/avatars/get-grocery-goods/2750890/4f6a267fd6db40bc9ca316f035b976f2/40x40-webp"
                         decoding="async" loading="eager" alt="Вода и напитки" class="i1y1fbtp" width="40" height="40"
                         role="presentation"> Вода и напитки
 
                 </a>
-                <a href="#">
+                <a href="frozen">
                     <img srcset="https://yastatic.net/avatars/get-grocery-goods/2750890/c6b09b3a8e784154ac2da9f300ca0816/40x40-webp 1x,https://yastatic.net/avatars/get-grocery-goods/2750890/c6b09b3a8e784154ac2da9f300ca0816/80x80-webp 2x"
                         src="https://yastatic.net/avatars/get-grocery-goods/2750890/c6b09b3a8e784154ac2da9f300ca0816/40x40-webp"
                         decoding="async" loading="eager" alt="Сладкое и снеки" class="i1y1fbtp" width="40" height="40"
@@ -54,28 +54,28 @@
 
 
                 </a>
-                <a href="#">
+                <a href="healthy-living">
                     <img srcset="https://yastatic.net/avatars/get-grocery-goods/2750890/3201a7d60b5642acafe22e44174815ea/40x40-webp 1x,https://yastatic.net/avatars/get-grocery-goods/2750890/3201a7d60b5642acafe22e44174815ea/80x80-webp 2x"
                         src="https://yastatic.net/avatars/get-grocery-goods/2750890/3201a7d60b5642acafe22e44174815ea/40x40-webp"
                         decoding="async" loading="eager" alt="Мясо, птица, рыба" class="i1y1fbtp" width="40" height="40"
                         role="presentation"> Мясо, птица, рыба
 
                 </a>
-                <a href="#">
+                <a href="grocery">
                     <img srcset="https://yastatic.net/avatars/get-grocery-goods/2750890/a9bafdc7c58b487f957b1f99e639e132/40x40-webp 1x,https://yastatic.net/avatars/get-grocery-goods/2750890/a9bafdc7c58b487f957b1f99e639e132/80x80-webp 2x"
                         src="https://yastatic.net/avatars/get-grocery-goods/2750890/a9bafdc7c58b487f957b1f99e639e132/40x40-webp"
                         decoding="async" loading="eager" alt="Заморозка" class="i1y1fbtp" width="40" height="40"
                         role="presentation"> Заморозка
 
                 </a>
-                <a href="#"><img
+                <a href="kids"><img
                         srcset="https://yastatic.net/avatars/get-grocery-goods/2750890/4be273adbb78492387226be8c0bc56c0/40x40-webp 1x,https://yastatic.net/avatars/get-grocery-goods/2750890/4be273adbb78492387226be8c0bc56c0/80x80-webp 2x"
                         src="https://yastatic.net/avatars/get-grocery-goods/2750890/4be273adbb78492387226be8c0bc56c0/40x40-webp"
                         decoding="async" loading="eager" alt="Здоровый образ жизни" class="i1y1fbtp" width="40"
                         height="40" role="presentation"> Здоровый образ жизни
 
                 </a>
-                <a href="#"><img
+                <a href="pets"><img
                         srcset="https://yastatic.net/avatars/get-grocery-goods/2750890/b002614ff95b41b499a4112860785e49/40x40-webp 1x,https://yastatic.net/avatars/get-grocery-goods/2750890/b002614ff95b41b499a4112860785e49/80x80-webp 2x"
                         src="https://yastatic.net/avatars/get-grocery-goods/2750890/b002614ff95b41b499a4112860785e49/40x40-webp"
                         decoding="async" loading="eager" alt="Бакалея" class="i1y1fbtp" width="40" height="40"
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,99 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Page from '@/pages/Page.vue'
-import HomeView from '@/views/HomeView.vue'
-import Login from '@/pages/Login.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "@/views/HomeView.vue";
+import ReadyMeals from "@/pages/ReadyMeals.vue";
+import Vegetables from "@/pages/Vegetables.vue";
+import Dairy from "@/pages/Dairy.vue";
+import Bakery from "@/pages/Bakery.vue";
+import Drinks from "@/pages/Drinks.vue";
+import Sweets from "@/pages/Sweets.vue";
+import MeatFish from "@/pages/MeatFish.vue";
+import Frozen from "@/pages/Frozen.vue";
+import HealthyLiving from "@/pages/HealthyLiving.vue";
+import Grocery from "@/pages/Grocery.vue";
+import Kids from "@/pages/Kids.vue";
+import Pets from "@/pages/Pets.vue";
+import HomeGoods from "@/pages/HomeGoods.vue";
+import Urgent from "@/pages/Urgent.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'HomeView',
+      path: "/",
+      name: "HomeView",
       component: HomeView,
     },
     {
-      path: '/page',
-      name: 'Page',
-      component: Page,
+      path: "/ready-meals",
+      name: "ReadyMeals",
+      component: ReadyMeals,
     },
     {
-      path: '/login',
-      name: 'Login',
-      component: Login,
+      path: "/vegetables",
+      name: "Vegetables",
+      component: Vegetables,
+    },
+    {
+      path: "/dairy",
+      name: "Dairy",
+      component: Dairy,
+    },
+    {
+      path: "/bakery",
+      name: "Bakery",
+      component: Bakery,
+    },
+    {
+      path: "/drinks",
+      name: "Drinks",
+      component: Drinks,
+    },
+    {
+      path: "/sweets",
+      name: "Sweets",
+      component: Sweets,
+    },
+    {
+      path: "/meat-fish",
+      name: "MeatFish",
+      component: MeatFish,
+    },
+    {
+      path: "/frozen",
+      name: "Frozen",
+      component: Frozen,
+    },
+    {
+      path: "/healthy-living",
+      name: "HealthyLiving",
+      component: HealthyLiving,
+    },
+    {
+      path: "/grocery",
+      name: "Grocery",
+      component: Grocery,
+    },
+    {
+      path: "/kids",
+      name: "Kids",
+      component: Kids,
+    },
+    {
+      path: "/pets",
+      name: "Pets",
+      component: Pets,
+    },
+    {
+      path: "/home-goods",
+      name: "HomeGoods",
+      component: HomeGoods,
+    },
+    {
+      path: "/urgent",
+      name: "Urgent",
+      component: Urgent,
     },
   ],
-})
+});
 
-export default router
+export default router;
